76294159b5e3a39330d69f08dbc825e680d8bdef
Somdeep Rana <131563091+i-am-zlatan@users.noreply.github.com>
2023-06-19 11:17:04 +0530

    golang-jwt-project
    This project encompasses the development of a comprehensive user authentication and authorization system in the Go programming language, leveraging JSON Web Tokens (JWT) for secure identity management. MongoDB, a popular NoSQL database, has been chosen as the backend database solution to store and manage user information efficiently.

---
+++ golang-jwt-project/routes/userRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "github.com/i-am-zlatan/golang-jwt-project/controllers"
+	"github.com/i-am-zlatan/golang-jwt-project/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
